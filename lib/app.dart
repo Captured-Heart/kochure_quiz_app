8adf4777251b2eb5ec78c0540addac74d480ec35
Captured-Heart <you@example.com>
2022-11-14 13:43:35 +0100

    changed file structure

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,15 @@
+//! UTILS 
 export './brand_colors.dart';
 export './utils/create_material_color.dart';
 export './utils/responsive.dart';
+export '../../../image_constants.dart';
+
+
+//! PACKAGES USED
+export 'package:flutter/material.dart';
+export 'package:flutter_riverpod/flutter_riverpod.dart';
+export 'package:animated_text_kit/animated_text_kit.dart';
+export 'package:google_fonts/google_fonts.dart';
+
+
+
