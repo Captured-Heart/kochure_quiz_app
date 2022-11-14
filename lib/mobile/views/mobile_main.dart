faf0869937411c6e1ebd38cd88ae97673b65ff77
Jeremiah Ekanem <jeremyessien@gmail.com>
2022-11-14 16:22:02 +0100

    Started building quiz screen

--- lib/mobile/views/mobile_main.dart
+++ lib/mobile/views/mobile_main.dart
@@ -1,6 +1,4 @@
-
 import '../../app.dart';
-import 'auth_screen_mobile.dart';
 
 class MobileMain extends ConsumerWidget {
   const MobileMain({super.key});
@@ -8,10 +6,47 @@ class MobileMain extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text('Skip question'),
+          )
+        ],
+      ),
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.all(20),
-
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 35,
+                decoration: BoxDecoration(
+                  border: Border.all(color: BrandColors.colorPrimaryDark),
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                ),
+                child: Stack(
+                  children: [
+                    LayoutBuilder(
+                      builder: (context, constraints) => Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.all(Radius.circular(20)),
+                            gradient: LinearGradient(colors: [
+                          BrandColors.colorPrimaryMaterial.shade300,
+                          BrandColors.colorPrimaryMaterial.shade500
+                        ])),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
