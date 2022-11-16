6f19158858fbbc8e3bc95c904ce602d4b7d64441
Captured-Heart <you@example.com>
2022-11-16 14:25:03 +0100

    added the timer counter. DONE

--- lib/desktop/view/auth_screen_desktop.dart
+++ lib/desktop/view/auth_screen_desktop.dart
@@ -1,4 +1,5 @@
 import 'package:form_field_validator/form_field_validator.dart';
+import 'package:kochure_quiz_app/desktop/view/quiz_screen_desktop.dart';
 
 import '../../app.dart';
 
@@ -119,6 +120,7 @@ class AuthScreenDesktopState extends ConsumerState<AuthScreenDesktop> {
                         width: size.width,
                         onTap: () {
                           // if (signUpKey.currentState!.validate()) {}
+                          pushNamed(context, QuizScreenDesktop.routeName);
                         },
                       ),
                     ]
@@ -149,11 +151,13 @@ class KochureButton extends StatelessWidget {
     required this.onTap,
     required this.text,
     this.width,
+    this.inActiveBtn,
   }) : super(key: key);
 
   final VoidCallback onTap;
   final String text;
   final double? width;
+  final bool? inActiveBtn;
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -162,29 +166,41 @@ class KochureButton extends StatelessWidget {
         alignment: Alignment.center,
         height: 50,
         width: width,
-        decoration: BoxDecoration(
-            gradient: RadialGradient(colors: [
-              BrandColors.colorPrimary,
-              BrandColors.colorPrimaryMaterial.shade300,
-              BrandColors.colorPrimary,
-              BrandColors.colorPrimaryMaterial.shade600,
-              BrandColors.colorPrimary,
-            ], stops: const [
-              0.1,
-              0.35,
-              0.5,
-              0.75,
-              0.95,
-            ], radius: 10),
-            borderRadius: const BorderRadius.all(Radius.circular(12)),
-            border: Border.all(
-              color: BrandColors.colorGreen,
-              width: 2,
-            )),
+        decoration: inActiveBtn == true
+            ? BoxDecoration(
+                color: BrandColors.colorLightGray.withOpacity(0.4),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(12),
+                ),
+              )
+            : BoxDecoration(
+                gradient: RadialGradient(colors: [
+                  BrandColors.colorPrimary,
+                  BrandColors.colorPrimaryMaterial.shade300,
+                  BrandColors.colorPrimary,
+                  BrandColors.colorPrimaryMaterial.shade600,
+                  BrandColors.colorPrimary,
+                ], stops: const [
+                  0.1,
+                  0.35,
+                  0.5,
+                  0.75,
+                  0.95,
+                ], radius: 10),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                border: Border.all(
+                  color: BrandColors.colorGreen,
+                  width: 2,
+                )),
         child: Text(
           text,
-          style: const TextStyle(
-              color: BrandColors.quickactionsBg, fontWeight: FontWeight.bold),
+          style: TextStyle(
+            color: inActiveBtn == true
+                ? BrandColors.colorPrimary
+                : BrandColors.quickactionsBg,
+            fontWeight:
+                inActiveBtn == true ? AppFontWeight.bold : AppFontWeight.bold,
+          ),
           textScaleFactor: 1.6,
         ),
       ),
--- lib/desktop/view/quiz_screen_desktop.dart
+++ lib/desktop/view/quiz_screen_desktop.dart
@@ -1,19 +1,52 @@
+import 'package:timer_count_down/timer_controller.dart';
+import 'package:timer_count_down/timer_count_down.dart';
+
 import '../../app.dart';
 
-class QuizScreenDesktop extends ConsumerWidget {
+// final timerProvider = Provider<Timer>((ref) {
+//   final stopWatch = Stopwatch()..start();
+
+//   return Timer.periodic(
+//     const Duration(seconds: 1),
+//     (value) {
+//       if (stopWatch.elapsedMilliseconds >= 10000) {
+//         log('onbooo');
+//       }
+//     },
+//   );
+// });
+
+final showButtonProvider = StateProvider<bool>((ref) {
+  return false;
+});
+final countDownControllerProvider = Provider<CountdownController>((ref) {
+  return CountdownController(autoStart: true);
+});
+
+class QuizScreenDesktop extends ConsumerStatefulWidget {
   static const String routeName = 'QuizScreen';
   const QuizScreenDesktop({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  QuizScreenDesktopState createState() => QuizScreenDesktopState();
+}
+
+class QuizScreenDesktopState extends ConsumerState<QuizScreenDesktop> {
+  @override
+  Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final no = ref.watch(pageIndexProvider);
     final pageController = ref.watch(pageViewControllerProvider);
+    final showButton = ref.watch(showButtonProvider);
+    // final timer = ref.watch(timerProvider);
+
+// fina
     return Scaffold(
       extendBodyBehindAppBar: true,
       body: SafeArea(
         child: QuizAppBackground(
           size: size,
+          imgPath: no.isOdd ? kochureBg5 : kochureBg4,
           child: Container(
             padding: const EdgeInsets.all(20),
             child: Column(
@@ -23,7 +56,68 @@ class QuizScreenDesktop extends ConsumerWidget {
                   height: size.height * 0.03,
                 ),
                 // Timer
-                const TimerCard(),
+                // const TimerCard(),
+                Countdown(
+                  seconds: 10,
+                  onFinished: () {
+                    ref
+                        .read(showButtonProvider.notifier)
+                        .update((state) => true);
+                  },
+                  build: (context, time1) {
+                    return Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        width: size.width * 0.5,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            border: Border.all(
+                              color: BrandColors.colorBackground,
+                              width: 3,
+                            )),
+                        padding: const EdgeInsets.only(left: 1),
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(30),
+                              child: LinearProgressIndicator(
+                                minHeight: 40,
+                                value: time1 / 10,
+                                // snapshot.data! / 10,
+                                valueColor: const AlwaysStoppedAnimation<Color>(
+                                    BrandColors.colorPrimaryDark),
+                                backgroundColor: Colors.transparent,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                // ignore: prefer_const_literals_to_create_immutables
+                                children: [
+                                  // ignore: prefer_const_constructors
+                                  Text(
+                                    '$time1 secs',
+                                    style: const TextStyle(
+                                        color: BrandColors.colorBackground),
+                                    textScaleFactor: 1.5,
+                                  ),
+                                  const Icon(
+                                    Icons.access_time_rounded,
+                                    color: Colors.white,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
                 const SizedBox(
                   height: 20,
                 ),
@@ -34,7 +128,7 @@ class QuizScreenDesktop extends ConsumerWidget {
                       fontSize: Responsive.isMobile(context) ? 18 : 25,
                       color: Colors.white,
                     ),
-                    children:  [
+                    children: [
                       TextSpan(
                         text: "/11",
                         style: TextStyle(
@@ -54,6 +148,7 @@ class QuizScreenDesktop extends ConsumerWidget {
                 ),
                 Expanded(
                   child: PageView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
                     onPageChanged: (value) {
                       ref
                           .read(pageIndexProvider.notifier)
@@ -70,16 +165,29 @@ class QuizScreenDesktop extends ConsumerWidget {
                     },
                   ),
                 ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: KochureButton(
-                    onTap: () {
-                      ref.read(pageViewControllerProvider).jumpToPage(no + 1);
-                    },
-                    text: 'Next',
-                    width: size.width * 0.25,
-                  ),
-                ),
+
+                showButton == false
+                    ? Align(
+                        alignment: Alignment.bottomRight,
+                        child: KochureButton(
+                          onTap: () {},
+                          text: 'Next',
+                          width: size.width * 0.25,
+                          inActiveBtn: true,
+                        ),
+                      )
+                    : Align(
+                        alignment: Alignment.bottomRight,
+                        child: KochureButton(
+                          onTap: () {
+                            ref
+                                .read(pageViewControllerProvider)
+                                .jumpToPage(no + 1);
+                          },
+                          text: 'Next',
+                          width: size.width * 0.25,
+                        ),
+                      ),
               ],
             ),
           ),
