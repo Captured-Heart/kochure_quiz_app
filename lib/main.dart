bab132076c65731bb514de5fdf95fab6e3101aa5
Captured-Heart <you@example.com>
2022-11-18 21:33:36 +0100

    added kochure icons

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Kochure Quiz',
+      
       debugShowCheckedModeBanner: false,
       // useInheritedMediaQuery: true,
       // builder: DevicePreview.appBuilder,
